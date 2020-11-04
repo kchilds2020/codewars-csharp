48231fe1a1d129f5d5627a0a1378cb4f200ebbe2
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-11-03 18:15:24 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeWarsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Kevin", "Nikki", "Taylor" };
+            string name = Likes.getLikes(arr);
+            Console.WriteLine(name);
+        }
+    }
+}
